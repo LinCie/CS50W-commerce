4a67d98128584498ac70bb037591b0e6cbcacc42
LinCie <91726589+LinCie@users.noreply.github.com>
2023-07-20 10:14:32 +0700

    added listing models and configured admin

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,27 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
+from django.core.validators import MinValueValidator
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+
+    name = models.CharField("listing name", max_length=50)
+    lister = models.ForeignKey(User, verbose_name=("lister name"), on_delete=models.CASCADE)
+    starting_bid = models.FloatField(("starting bid"), validators=[MinValueValidator(0.01)])
+    image_url = models.URLField(("image url"), max_length=256)
+    description = models.TextField(("listing description"))
+    
+    class Meta:
+        verbose_name = ("listing")
+        verbose_name_plural = ("listings")
+
+    def __str__(self):
+        return f"{self.name} by {self.lister}"
+
+    def get_absolute_url(self):
+        return reverse("listing_detail", kwargs={"pk": self.pk})
